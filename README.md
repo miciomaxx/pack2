7600697281d80a0c01c7c62431ef77bb47c9342a
Mo DieSse <salsasiempre90@gmail.com>
2020-08-16 18:08:40 +0200

    new version 3.0.0

--- README.md
+++ README.md
@@ -2,19 +2,27 @@
 
 csrpacker2 
 
-instructions:
-- need python installed;
-- no needs to open cmd dos prompt;
-- all library packed in onefile;
-- put csrpacker2.py in a directory of your choice;
-- put in nsb, scb and 8ed9e902c5c024bfb899e99893d4eb525d3ad179 files as they come from csr2 game;
-- double click on csrpacker2.py;
-- original files are renamed as .old and put in Original folder;
-- new files are created as .txt in Decrypted folder; they are in ready prettyfied text;
-- after modifying .txt, save them and don't rename them; no needs to minify;
-- double click on csrpacker2.py;
-- new files are created without extension ready to use in Finished folder;
-- crc32 is automatically calculated and inserted on right file and "temp" user is automatically deleted;
-- check for json errors;
-- log.txt is created and contains log for check and issues report.
+versions:
+- csrpacker2.py (need python installed) 
+- csrpacker2.exe (all library packed in onefile)
 
+features:
+- no needs to open cmd dos prompt
+- managing (pack and/or unpack) nsb, scb, 8ed and trb, alone or not
+- check for json errors
+
+
+instructions:
+- put csrpacker2.py in a directory of your choice;
+  1st pass:
+  - put in nsb, scb, 8ed9e902c5c024bfb899e99893d4eb525d3ad179 or trb files as they come from csr2 game;
+  - double click on csrpacker2;
+  - original files are renamed as .old and put in Original folder;
+  - new files are created as .txt in Decrypted folder; they are in ready prettyfied text;
+  2nd pass:
+  - after modifying .txt, save them and don't rename them; no needs to minify;
+  - double click on csrpacker2.py;
+  - new files are created without extension ready to use in Finished folder;
+  - log.txt is created and contains log for check and issues report.
+
+If you only have to pack files, name them as .txt and put them in Decrypted folder
--- csrpacker2.py
+++ csrpacker2.py
@@ -1,20 +1,19 @@
 from hashlib import sha1
 from hmac import new
 from sys import exit
-from os import path, rename, makedirs
+from os import path, rename, makedirs, remove
 from gzip import open as open1
 from json import loads, dumps
-from zlib import crc32
 
 writer2 = open('log.txt', 'w')
 
-workdone = False
-nsbcrc32 = 0
+packdone = False
+
 
 def spack(fileuno):
     with open1(fileuno, 'rb') as f:
-            file_content = f.readline()
-            file_content = f.readline()
+        file_content = f.readline()
+        file_content = f.readline()
 
     parsed = loads(file_content)
     file_pret = dumps(parsed, indent=4, sort_keys=False)
@@ -22,7 +21,8 @@ def spack(fileuno):
     writer1 = open('Decrypted/'+fileuno+'.txt', 'w', encoding='utf8')
     writer1.write(file_pret)
     writer1.close()
-    
+
+
 def pack(fileuno):
     writer1 = open('Decrypted/'+fileuno + '.txt', 'r', encoding='utf8')
     file_pret = writer1.read()
@@ -38,83 +38,66 @@ def pack(fileuno):
     if fileuno == '8ed9e902c5c024bfb899e99893d4eb525d3ad179':
         try:
             json_data['profileSaveHashes'] = {}
-            json_data['profileSaveHashes'] = {'playerID': json_data['userid'], 'CRC': str(nsbcrc32)}
+            json_data['profileSaveHashes'] = {'playerID': json_data['userid'], 'CRC': ""}
             writer2.write(': crc updated')
         except:
             writer2.write(': crc not updated')
 
     json_string = dumps(json_data, ensure_ascii=True, separators=(',', ":"))
     file4crc32 = json_string.encode('utf8')
-    crc32_file = crc32(file4crc32)
     secretkeybytes = bytearray([52, 99, 80, 119, 51, 90, 121, 67])
     dig2 = new(secretkeybytes, file4crc32, sha1).hexdigest()
+
+    try:    # Maybe file not exist
+        remove('Finished/'+fileuno)
+    except:
+        pass
 
     with open1('Finished/'+fileuno, 'a') as f2:
         f2.write(bytes(dig2, 'utf8'))
         f2.write(bytes('\n', 'utf8'))
         f2.write(file4crc32)
         f2.close()
-    return crc32_file
 
-if path.exists('nsb'):
-    if not path.exists('Original'):
-        makedirs('Original')
-    if not path.exists('Decrypted'):
-        makedirs('Decrypted')
 
-    writer2.write('start: trying to unpack nsb...')
-    spack('nsb')
-    rename('nsb', 'Original/nsb.old')
-    writer2.write('done!\n\r')
-    workdone = True
-else:
-    writer2.write('missing original nsb file\n\r')
+files = ['nsb', 'scb', '8ed9e902c5c024bfb899e99893d4eb525d3ad179', 'trb']
 
-if path.exists('scb'):
-    writer2.write('trying to unpack nsb...')
-    spack('scb')
-    rename('scb', 'Original/scb.old')
-    writer2.write('done!\n\r')
-else:
-    writer2.write('missing original scb file\n\r')
+if not path.exists('Original'):
+    makedirs('Original')
+if not path.exists('Decrypted'):
+    makedirs('Decrypted')
 
-if path.exists('8ed9e902c5c024bfb899e99893d4eb525d3ad179'):
-    writer2.write('trying to unpack 3rd...')
-    spack('8ed9e902c5c024bfb899e99893d4eb525d3ad179')
-    rename('8ed9e902c5c024bfb899e99893d4eb525d3ad179', 'Original/8ed9e902c5c024bfb899e99893d4eb525d3ad179.old')
-    writer2.write('done!\n\r')
-else:
-    writer2.write('missing original 3rd file: ok if you are on Ios\n\r')
+for singleFile in files:
+    if path.exists(singleFile):
+        writer2.write('start: trying to unpack %s...' % singleFile)
+        spack(singleFile)
+        try:    # Maybe file not exist
+            remove('Original/%s.old' % singleFile)
+        except:
+            pass
+        rename(singleFile, 'Original/%s.old' % singleFile)
+        writer2.write('done!\n\r')
+        packdone = True
+    else:
+        writer2.write('missing original %s file\n\r' % singleFile)
 
-if workdone:
+if packdone:
     writer2.write('unpacking completed\n\r\n\r')
     writer2.close()
     exit()
-    
-if path.exists('Decrypted/nsb.txt'):
-    writer2.write('nothing to unpack: trying to pack nsb.txt file')
-    if not path.exists('Finished'):
-        makedirs('Finished')
-    nsbcrc32 = pack('nsb')
-    writer2.write(': done\n\r')
 
-else:
-    writer2.write(': missing nsb.txt file\n\r')
+if not path.exists('Finished'):
+    makedirs('Finished')
 
-if path.exists('Decrypted/scb.txt'):
-    writer2.write('trying to pack scb.txt file')
-    pack('scb')
-    writer2.write(': done\n\r')
+writer2.write('nothing to unpack:')
 
-else:
-    writer2.write(': missing scb.txt file\n\r')
+for singleFile in files:
+    if path.exists('Decrypted/%s.txt' % singleFile):
+        writer2.write(' trying to pack %s.txt file' % singleFile)
+        pack(singleFile)
+        writer2.write(': done\n\r')
 
-if path.exists('Decrypted/8ed9e902c5c024bfb899e99893d4eb525d3ad179.txt'):
-    writer2.write('trying to pack 3rd file')
-    pack('8ed9e902c5c024bfb899e99893d4eb525d3ad179')
-    writer2.write(': done\n\r')
-
-else:
-    writer2.write(': missing 3rd file\n\r')
+    else:
+        writer2.write(': missing %s.txt file\n\r' % singleFile)
 
 writer2.close()
